82bafa5729524053f69f2d042ef5ee0acb54d6e8
Regent Yao Agbemafle <regent.ay7@gmail.com>
2026-05-24 01:13:29 -0600

    Tokenization implemented with python

--- 14_generative_ai/01_tokenization.py
+++ 14_generative_ai/01_tokenization.py
@@ -1,7 +1,17 @@
+import tiktoken
+
+enc = tiktoken.encoding_for_model("gpt-4o")
 
 
+text = "Hello, welcome to my first LLM application"
+tokens = enc.encode(text)
+print(tokens)
 
 
+decoder = enc.decode([13225, 11, 12591, 316, 922, 1577, 451, 19641, 5200])
+
+
+print(decoder)
 
 
 
