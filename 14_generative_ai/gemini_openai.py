5654304f3ddde47f4ba6608c4566539a44a23d49
Regent Yao Agbemafle <regent.ay7@gmail.com>
2026-05-24 15:37:38 -0600

    Helps to run the gemini model from openai

--- 14_generative_ai/gemini_openai.py
+++ 14_generative_ai/gemini_openai.py
@@ -3,9 +3,12 @@ from openai import OpenAI
 
 
 load_dotenv()
-client = OpenAI()
+client = OpenAI(
+    api_key ="AIzaSyC5EB9Kmf-N8xDgkenas0gCQn9CRxbBMlM",
+    base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 response= client.chat.completions.create(
-    model="gpt-4o-mini",
+    model="gemini-2.5-flash",
     messages= [
         {"role" : "user", "content": "Hey, I  Regent and nice to meet you "}
     ]
